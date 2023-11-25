dc264c1912a28a125ee42d46c136bd04e199d12d
Egor <jeltok88@yandex.ru>
2023-11-25 21:58:56 +0300

    Добавлен способ добавлять несколько хэш-значений в массив

--- pokemon.rb
+++ pokemon.rb
@@ -1,5 +1,6 @@
 def pokeball(number)
   pokearray = []
+  arraypok = []
   number.times do
     puts "Введите имя покемона: "
     name = gets.chomp
@@ -7,8 +8,9 @@ def pokeball(number)
     puts "Введите цвет покемона: "
     color = gets.chomp
     pokearray = [{name: name, color: color}]
+    arraypok = arraypok + pokearray
   end
-  puts pokearray
+  puts arraypok
 end
 
 puts "Сколько покемонов добавим? Введите число: "
